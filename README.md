3e55b21cf9c54a70812860845d9404896570aa6c
Rich Turnbull <rpturnbull1@gmail.com>
2019-07-03 21:51:05 +0100

    v3 secure method for use with koa endpoint

--- README.md
+++ README.md
@@ -6,16 +6,26 @@
 ```
 const Policy = require('@hotelflex/policy')
 
-const policy = new Policy()
-  .allowRoot()
-  .allowHotelier('MEMBER')
+const policy = Policy({
+  hotel: ['MEMBER'],
+  hotelflex: 'ACCOUNT_MANAGER',
+  sessionOwner: true,
+  accountOwner: true,
+  root: true, //defaults to true
+  public: true, //defaults to false
+})
+
+policy.load(session)
+// => this will boom if session doesn't hold any of the stated permissions
+
+policy.apply(opts)
+// => Use this to check specific params against permissions
+
+const opts = {
+  hotelId: '123',
+  sessionId: 'sess1',
+  accountId: 'acc1',
+  ...
+}
+
 ```
-
-Supported methods
-
-* allowPublic
-* allowRoot
-* allowAccountOwner
-* allowSessionOwner
-* allowHotelier
-* allowHotelflex
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const PolicyError = require('./PolicyError')
+const Boom = require('@hapi/boom')
 
 function normalizeSession(session) {
   if (!session || session !== Object(session)) return { permissions: [] }
@@ -6,104 +6,116 @@ function normalizeSession(session) {
   if (!Array.isArray(session.permissions)) {
     session.permissions = []
   }
-
   return session
 }
 
-class Policy {
-  constructor() {
-    this.exec = this.exec.bind(this)
-    this.exec.allowRoot = this.allowRoot.bind(this)
-    this.exec.allowPublic = this.allowPublic.bind(this)
-    this.exec.allowSessionOwner = this.allowSessionOwner.bind(this)
-    this.exec.allowAccountOwner = this.allowAccountOwner.bind(this)
-    this.exec.allowHotelflex = this.allowHotelflex.bind(this)
-    this.exec.allowHotelier = this.allowHotelier.bind(this)
-
-    this._allowRoot = false
-    this._allowPublic = false
-    this._allowSessionOwner = false
-    this._hotelflexRoles = []
-    this._hotelierRoles = []
-
-    return this.exec
-  }
-
-  allowRoot() {
-    this._allowRoot = true
-    return this.exec
-  }
-
-  allowPublic() {
-    this._allowPublic = true
-    return this.exec
-  }
-
-  allowSessionOwner() {
-    this._allowSessionOwner = true
-    return this.exec
-  }
-
-  allowAccountOwner() {
-    this._allowAccountOwner = true
-    return this.exec
-  }
-
-  allowHotelflex(role) {
-    this._hotelflexRoles.push(role)
-    return this.exec
-  }
-
-  allowHotelier(role) {
-    this._hotelierRoles.push(role)
-    return this.exec
-  }
-
-  exec(session, opts = {}) {
-    session = normalizeSession(session)
-
-    if (this._allowPublic) return
-    if (this._allowRoot && session.isRoot) return
-
-    if (
-      this._allowSessionOwner &&
-      session.id === opts.sessionId &&
-      opts.sessionId
-    )
-      return
-
-    if (
-      this._allowAccountOwner &&
-      session.accountId === opts.accountId &&
-      opts.accountId
-    )
-      return
-
-    for (let i = 0; i < this._hotelflexRoles.length; i++) {
-      const role = this._hotelflexRoles[i]
-      const matches = session.permissions.filter(
-        p => p.type === 'HOTELFLEX' && p.role === role,
-      )
-
-      if (matches.length > 0) return
-    }
-
-    for (let i = 0; i < this._hotelierRoles.length; i++) {
-      const role = this._hotelierRoles[i]
-      const matches = session.permissions.filter(
-        p =>
-          p.entityId === opts.hotelId &&
-          p.type === 'HOTELIER' &&
-          p.role === role,
-      )
-
-      if (matches.length > 0) return
-    }
-
-    throw new PolicyError()
+function secure(config, handler) {
+  const policy = new Policy(config)
+  return function(ctx, next) {
+    policy.load(ctx.request.session).validate()
+    return handler(ctx, next, policy)
   }
 }
 
-Policy.Error = PolicyError
+class Policy {
+  constructor(config={}) {
+    this.from = this.from.bind(this)
+    this.load = this.load.bind(this)
+    this.apply = this.apply.bind(this)
+    this.validate = this.validate.bind(this)
 
+    this.isRoot = false
+    this.accountId = null
+    this.sessionId = null
+    this.hotelPermissions = []
+    this.hotelflexPermissions = []
+
+    this._allowRoot = true
+    this._allowPublic = false
+    this._allowSessionOwner = false
+    this._allowAccountOwner = false
+    this._hotelflexRoles = []
+    this._hotelRoles = []
+    this.from(config)
+  }
+  from(config={}) {
+    if(config.hotel) {
+      if(typeof config.hotel === 'string') {
+        this._hotelRoles.push(config.hotel)
+      } else {
+        this._hotelRoles.push(...config.hotel)
+      }
+    }
+    if(config.hotelflex) {
+      if(typeof config.hotelflex === 'string') {
+        this._hotelRoles.push(config.hotelflex)
+      } else {
+        this._hotelRoles.push(...config.hotelflex)
+      }
+    }
+    if(config.accountOwner !== undefined) {
+      this._allowAccountOwner = Boolean(config.accountOwner)
+    }
+    if(config.sessionOwner !== undefined) {
+      this._allowSessionOwner = Boolean(config.sessionOwner)
+    }
+    if(config.root !== undefined) {
+      this._allowRoot = Boolean(config.root)
+    }
+    if(config.public !== undefined) {
+      this._allowPublic = Boolean(config.public)
+    }
+    return this
+  }
+  load(session) {
+    session = normalizeSession(session)
+    this.session = session
+    if(this._allowRoot && session.isRoot) {
+      this.isRoot = true
+    }
+    if(this._allowSessionOwner && session.id) {
+      this.sessionId = session.id
+    }
+    if(this._allowAccountOwner && session.accountId) {
+      this.accountId = session.accountId
+    }
+    if(this._hotelRoles.length > 0) {
+      this.hotelPermissions = session.permissions
+        .filter(p => this._hotelRoles.indexOf(p.role) > -1)
+    }
+    if(this._hotelflexRoles.length > 0) {
+      this.hotelflexPermissions = session.permissions
+        .filter(p => this._hotelflexRoles.indexOf(p.role) > -1)
+    }
+    return this
+  }
+  validate() {
+    if(this._allowPublic) return
+    if(!this.session || !this.session.id) 
+      throw Boom.unauthorized()
+    if(
+      !this.isRoot
+      && !this.sessionId
+      && !this.accountId
+      && this.hotelPermissions.length === 0
+      && this.hotelflexPermissions.length === 0
+    ) throw Boom.forbidden()
+    return this
+  }
+  apply(opts={}) {
+    const { sessionId, accountId, hotelId } = opts
+    if(this.isRoot) return
+    if(this.hotelflexPermissions.length > 0) return
+    if(this.sessionId && this.sessionId === sessionId) return
+    if(this.accountId && this.accountId === accountId) return
+    if(this.hotelPermissions.length > 0 && hotelId) {
+      for (var i = this.hotelPermissions.length - 1; i >= 0; i--) {
+        const p = this.hotelPermissions[i]
+        if(p.entityId === hotelId) return
+      }
+    }
+    throw Boom.forbidden()
+  }
+}
+Policy.secure = secure
 module.exports = Policy
